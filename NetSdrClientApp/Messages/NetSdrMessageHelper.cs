70cc49d0384a96ba2b181a5c0bc6637a3f0d56bd
PS-AuraTrash-K <taras.shparuk@gmail.com>
2025-11-18 15:41:34 +0200

    Split L109 method into two, one handling parameters check and the other handling the iterator

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -108,25 +108,32 @@ namespace NetSdrClientApp.Messages
 
         public static IEnumerable<int> GetSamples(ushort sampleSize, byte[] body)
         {
-            sampleSize /= 8; //to bytes
-            if (sampleSize  > 4)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
+            int bytesCount = sampleSize / 8;
 
-            var bodyEnumerable = body as IEnumerable<byte>;
-            var prefixBytes = Enumerable.Range(0, 4 - sampleSize)
-                                      .Select(b => (byte)0);
+            if (bytesCount > 4)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sampleSize));
+            }
+            ArgumentNullException.ThrowIfNull(body);
+
+            return GetSamplesIterator(bytesCount, body);
+        }
+
+        private static IEnumerable<int> GetSamplesIterator(int sampleSize, IEnumerable<byte> bodyEnumerable)
+        {
+            var prefixBytes = Enumerable.Repeat((byte)0, 4 - sampleSize);
 
             while (bodyEnumerable.Count() >= sampleSize)
             {
                 yield return BitConverter.ToInt32(bodyEnumerable
                     .Take(sampleSize)
                     .Concat(prefixBytes)
-                    .ToArray());
+                    .ToArray(), 0);
+
                 bodyEnumerable = bodyEnumerable.Skip(sampleSize);
             }
         }
+        
 
         private static byte[] GetHeader(MsgTypes type, int msgLength)
         {
